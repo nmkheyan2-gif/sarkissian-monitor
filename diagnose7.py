faa3003f0f2f4a7a7a78ee9d7fcbc1c219f24788
nmkheyan2-gif <nmkheyan2@gmail.com>
2026-07-24 12:31:44 +0400

    Update diagnose7.py

--- diagnose7.py
+++ diagnose7.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup, NavigableString
 
-URL = "https://www.sarkissian.ru/catalog/sergi/turmalin/"
+URL = "https://www.sarkissian.ru/product/serebryanoe-kole-eliksir-smelosti/"
 
 headers = {
     "User-Agent": (
